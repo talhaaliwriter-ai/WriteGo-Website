1ad726d9268cc58ba35e39349a1ce24a342dafb1
talhaaliwriter-ai <talhaaliwriter@gmail.com>
2026-08-12 22:47:42 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import About from "./pages/About";
 import ServicesPage from "./pages/ServicesPage";
+import ServiceDetail from "./pages/ServiceDetail";
 import Portfolio from "./pages/Portfolio";
 import Blog from "./pages/Blog";
 import Pricing from "./pages/Pricing";
@@ -14,14 +16,26 @@ export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+
       <Route path="/about" element={<About />} />
+
       <Route path="/services" element={<ServicesPage />} />
+
+      {/* Individual Service Pages */}
+      <Route path="/services/:slug" element={<ServiceDetail />} />
+
       <Route path="/portfolio" element={<Portfolio />} />
+
       <Route path="/blog" element={<Blog />} />
+
       <Route path="/pricing" element={<Pricing />} />
+
       <Route path="/contact" element={<Contact />} />
+
       <Route path="/privacy-policy" element={<Privacy />} />
+
       <Route path="/terms" element={<Terms />} />
+
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
